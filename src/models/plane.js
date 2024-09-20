9e3a0585f97cda0f85acb06370be51766ee6c745
Leopoldo López <leopoldo.misael.lopez.de.alba@gmail.com>
2024-09-20 11:49:27 -0600

    added models to create subscription

--- src/models/plane.js
+++ src/models/plane.js
@@ -47,6 +47,85 @@ const SubscriptionPrice = db.sequelize.define(
     { timestamps: false, underscored: true }
 );
 
+const PaymentPlatform = db.sequelize.define(
+    'payment_platforms',
+    {
+        name: DataTypes.STRING,
+        state: DataTypes.CHAR
+    }
+);
+
+const PaymentMethod = db.sequelize.define(
+    'payment_platforms',
+    {
+        name: DataTypes.STRING,
+        state: DataTypes.CHAR
+    }
+);
+
+const UserPaymentPlatform = db.sequelize.define(
+    'user_payment_platforms',
+    {
+        userId: DataTypes.INTEGER,
+        paymentPlatformId: DataTypes.INTEGER,
+        referenceId: DataTypes.STRING,
+        state: DataTypes.CHAR
+    },
+    { updatedAt: false, underscored: true }
+);
+
+const Subscription = db.sequelize.define(
+    'subscriptions',
+    {
+        userId: DataTypes.INTEGER,
+        planeId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'planes',
+                key: 'id'
+            }
+        },
+        subscriptionTypeId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'subscription_types',
+                key: 'id'
+            }
+        },
+        paymentMethodId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'payment_methods',
+                key: 'id'
+            }
+        },
+        paymentPlatformId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'payment_platforms',
+                key: 'id'
+            }
+        },
+        hasTrialDays: DataTypes.BOOLEAN,
+        state: DataTypes.CHAR,
+        endDate: DataTypes.DATE
+    },
+    { underscored: true }
+);
+
+const SubscriptionPeriod = db.sequelize.define(
+    'subscription_periods',
+    {
+        subscriptionId: DataTypes.INTEGER,
+        price: DataTypes.FLOAT,
+        referenceId: DataTypes.STRING,
+        errorDetails: DataTypes.JSON,
+        state: DataTypes.CHAR,
+        startDate: DataTypes.DATE,
+        endDate: DataTypes.DATE
+    },
+    { underscored: true }
+);
 
 Plane.hasMany(Benefit);
 Plane.hasMany(SubscriptionPrice);
@@ -57,5 +136,10 @@ module.exports = {
     Plane,
     Benefit,
     SubscriptionType,
-    SubscriptionPrice
-}
+    SubscriptionPrice,
+    PaymentPlatform,
+    PaymentMethod,
+    UserPaymentPlatform,
+    Subscription,
+    SubscriptionPeriod
+};
