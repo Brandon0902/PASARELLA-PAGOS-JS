6c777661ef877f916e8a553cdcd70090767a2b33
Leopoldo López <leopoldo.misael.lopez.de.alba@gmail.com>
2024-09-20 11:48:06 -0600

    added migrations

---
+++ migrations/20240920020313-create-payment-method.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('payment_methods', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: 'VARCHAR(30)'
+      },
+      state: {
+        allowNull: false,
+        type: 'VARCHAR(10)'
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('payment_methods');
+  }
+};
---
+++ migrations/20240920023300-create-subscription-period.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('subscription_periods', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subscription_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'subscriptions',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      reference_id: {
+        allowNull: false,
+        type: 'VARCHAR(50)'
+      },
+      error_details: {
+        type: Sequelize.JSON
+      },
+      state: {
+        allowNull: false,
+        type: 'VARCHAR(15)'
+      },
+      start_date: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      end_date: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('subscription_periods');
+  }
+};
