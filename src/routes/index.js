513a8607c756932b24ff87fd47999e283a8e70cf
Leopoldo López <leopoldo.misael.lopez.de.alba@gmail.com>
2024-09-26 19:57:48 -0600

    added auth middleware to subscription route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const PlaneController = require('../controllers/planeController')
 const SubscriptionController = require('../controllers/subscriptionController')
+const { checkAuthToken } = require('../middlewares/auth')
 
 router.get('/planes', PlaneController.getAll)
 
-router.post('/subscriptions', SubscriptionController.create)
+router.post('/subscriptions', checkAuthToken, SubscriptionController.create)
 
 router.get('/', (req, res, next) => {
   res.json({ message: 'Hello World A Payments Legalian' });
