30264fdf57a9ecaefecd15ca7618999a6339042f
Brandon-Lopez <brandon.lopez@subl.ai>
2024-09-28 14:43:18 -0600

    platform_payment_id field renaming

--- migrations/20240920193815-create-plane-payment-platforms.js
+++ migrations/20240920193815-create-plane-payment-platforms.js
@@ -24,7 +24,7 @@ module.exports = {
           key: 'id'
         }
       },
-      platform_payment_id: {
+      payment_platform_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
