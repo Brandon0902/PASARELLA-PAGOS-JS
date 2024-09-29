0f98b44208eee5e49180d87f0a1f3621d7c3c3a7
Brandon-Lopez <brandon.lopez@subl.ai>
2024-09-28 19:03:19 -0600

    connecta implementation

--- src/services/paymentPlatformService.js
+++ src/services/paymentPlatformService.js
@@ -1,35 +1,43 @@
 const { PaymentPlatform, PlanePaymentPlatform } = require('../models/plane');
-const conektaService = require('./interfazService');
+const conektaService = require('./conektaService');
 
 async function processPaymentPlatforms(customerData, paymentType) {
     const { planeId, subscriptionTypeId } = customerData; 
 
     try {
-        // Obtener plataformas de pago filtradas por plane_id y subscription_type_id
-        const paymentPlatform = await PlanePaymentPlatform.findAll({
+        // Obtener plataformas de pago filtradas por planeId y subscriptionTypeId
+        const paymentPlatforms = await PlanePaymentPlatform.findAll({
             where: {
-                plane_id: planeId,  
-                subscription_type_id: subscriptionTypeId
+                planeId,
+                subscriptionTypeId
             },
             include: [{
                 model: PaymentPlatform,
-                attributes: ['id', 'name', 'state', 'created_at']
+                attributes: ['id', 'name', 'state', 'createdAt']
             }]
         });
 
         // Recorrer cada plataforma de pago
-        for (let platform of paymentPlatform) {
-            const paymentPlatformId = platform.payment_platform_id; 
-            const referenceId = platform.reference_id;
+        for (let platform of paymentPlatforms) {
+            const paymentPlatformId = platform.paymentPlatformId; 
+            const referenceId = platform.referenceId;
 
             if (paymentPlatformId === 1) {
                 console.log('Procesando Conekta con referenceId:', referenceId);
-                
-                await conektaService.proccessCustomerData(customerData, referenceId, paymentType);
+
+                // Llamar a createSubscription y capturar los valores
+                const { customerId, subscriptionId } = await conektaService.createSubscription(customerData, referenceId, paymentType);
 
                 console.log('Integración con Conekta completada.');
+
+                return {
+                    customerId,
+                    subscriptionId,
+                    platformId: paymentPlatformId 
+                };
             }
         }
+
     } catch (error) {
         console.error('Error al procesar las plataformas de pago:', error);
         throw error;
