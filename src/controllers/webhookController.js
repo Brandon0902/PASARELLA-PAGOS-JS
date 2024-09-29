5f1b42eda8f6dd58fa7c9fbe1f93761e235ed2eb
Brandon-Lopez <brandon.lopez@subl.ai>
2024-09-29 14:53:15 -0600

    strategy to process events

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -1,60 +1,17 @@
 const processEventService = require('../services/processEventService');
 
-const processSubscriptionPaid = async (event) => {
-    const subscriptionPaidData = {
-        id: event.id,
-        status: event.status,
-        chargeId: event.charge_id,
-        customerId: event.customer_id,
-        billingCycleStart: event.billing_cycle_start,
-        billingCycleEnd: event.billing_cycle_end,
-        subscriptionStart: event.subscription_start,
-        lastBillingCycleOrderId: event.last_billing_cycle_order_id
-    };
-
-    // Llamada al servicio para manejar el pago de suscripción
-    await processEventService.handleSubscriptionPaid(subscriptionPaidData);
-};
-
-const processChargeDeclined = async (event) => {
-    const chargeDeclinedData = {
-        chargeId: event.charge_id,
-        customerId: event.customer_id,
-        subscriptionId: event.id,
-        reason: event.decline_reason
-    };
-
-    // Llamada al servicio para manejar el rechazo del cargo
-    await processEventService.handleChargeDeclined(chargeDeclinedData);
-};
-
-// Mapa de funciones por tipo de evento
-const eventHandlers = {
-    'subscription.paid': processSubscriptionPaid,
-    'charge.declined': processChargeDeclined
-};
-
-
-const handleWebhook = async (req, res, next) => {
+const handleWebhook = async (req, res) => {
     const event = req.body;
 
     try {
-        // Obtener el tipo de evento
         const eventType = event.type;
-
-        const eventHandler = eventHandlers[eventType];
-
-        if (eventHandler) {
-            await eventHandler(event);
-            res.status(200).send('Evento recibido y procesado');
-        } else {
-            res.status(400).send(`Evento no manejado: ${eventType}`);
-        }
+        await processEventService.executeStrategy(eventType, event);
+        res.status(200).send('Evento recibido y procesado');
     } catch (error) {
-        res.status(500).send('Error procesando el evento');
+        res.status(400).send(`Error procesando el evento: ${error.message}`);
     }
 };
 
 module.exports = {
-    handleWebhook
+    handleWebhook,
 };
--- src/services/processEventService.js
+++ src/services/processEventService.js
@@ -1,12 +1,25 @@
 const handleSubscriptionPaid = async (subscriptionPaidData) => {
-
-};
-
-const handleChargeDeclined = async (chargeDeclinedData) => {
     
 };
 
+const handleChargeDeclined = async (chargeDeclinedData) => {
+
+};
+
+
+const strategies = {
+    'subscription.paid': handleSubscriptionPaid,
+    'charge.declined': handleChargeDeclined,
+};
+
+const executeStrategy = async (eventType, data) => {
+    const strategy = strategies[eventType];
+    if (!strategy) {
+        throw new Error(`Estrategia no encontrada para el evento: ${eventType}`);
+    }
+    return await strategy(data);
+};
+
 module.exports = {
-    handleSubscriptionPaid,
-    handleChargeDeclined
+    executeStrategy,
 };
