94787dde7d27b2ecf817417e991008a4f9bc18dc
Leopoldo López <leopoldo.misael.lopez.de.alba@gmail.com>
2024-10-08 20:55:27 -0600

    remove auth apikey middleware of webhook

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const webhookController = require('../controllers/webhookController');
-const { checkApiKey } = require('../middlewares/auth')
 
-router.post('/webhook', checkApiKey, webhookController.handleWebhook);
+router.post('/webhook', webhookController.handleWebhook);
 
 module.exports = router;
