f414c7093c85dad43d2313d8929e8797ce6e0654
Brandon-Lopez <brandon.lopez@subl.ai>
2024-10-11 19:25:45 -0600

    route update

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ router.post('/plane-payment-platforms', PlaneController.createPaymentPlatform)
 
 router.post('/subscriptions', checkAuthToken, SubscriptionController.create)
 router.post('/subscriptions/:id(\\d+)/cancel', checkAuthToken, SubscriptionController.cancel)
-router.get('/subscriptions/active', checkAuthToken, SubscriptionController.getActiveSubscription);
+router.get('/subscriptions', checkAuthToken, SubscriptionController.getActiveSubscription);
 
 router.get('/', (req, res, next) => {
   res.json({ message: 'Hello World A Payments Legalian' });
