0ebc6d95d01d826abc573f727654c02df33df8dc
César Díaz <cesar@bowheadhealth.com>
2024-10-26 20:46:33 -0600

    Update database configuration to enable SSL connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,30 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+let extra_params = {};
+
+if (process.env.DB_REQUIRE_SSL === 'true') {
+  extra_params = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+      ca: fs.readFileSync('./src/certs/us-east-1-bundle.pem').toString(),
+    }
+  }
+}
+
 const sequelize = new Sequelize(
-  process.env.POSTGRESQL_DATABASE, 
-  process.env.POSTGRESQL_USERNAME, 
-  process.env.POSTGRESQL_PASSWORD, 
+  process.env.POSTGRESQL_DATABASE,
+  process.env.POSTGRESQL_USERNAME,
+  process.env.POSTGRESQL_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
     timezone: '+00:00',
     dialectOptions: {
-      useUTC: true
+      useUTC: true,
+      ...extra_params
     }
   }
 );
