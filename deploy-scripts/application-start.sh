060233479b4dabe8703f7de07d0a4fa47d814c86
Brandon De Alba <115058579+Brandon0902@users.noreply.github.com>
2024-12-02 13:27:27 -0600

    Delete deploy-scripts directory

--- deploy-scripts/application-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# application-start.sh
-# Legalian script to start application after automatic deploy using AWS CodeDeploy
-
-# Dependencies
-su - pm2 -c "cd  /opt/pm2/pasarela && npm install"
-
-#Start services
-systemctl start pm2-pm2.service
--- deploy-scripts/application-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# application-stop.sh
-# Legalian script to stop application before automatic deploy using AWS CodeDeploy
-
-systemctl stop pm2-pm2.service
